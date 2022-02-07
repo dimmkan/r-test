d61af95318f9864378064cdb6f1359c742b3a88f
dimmkan <dimmkan@mail.ru>
2022-02-07 14:52:44 +0300

    Fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const goodsRouter = require('./routes/goods');
 const app = express();
 const  LISTEN_PORT = 3003;
 
-app.use(cors());
+app.use(cors);
 app.options('*', cors);
 app.use(express.json());
 app.use('/api/categories', categoriesRouter);
